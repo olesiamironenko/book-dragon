0f85b97134951bdb6b96a14e0c4d9784be621db5
Olesia Mironenko <olesia.a.mironenko@gmail.com>
2024-10-04 20:34:13 -0400

    users created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
 
   resources :authors do
     get 'books', to: 'authors#books', as: 'author_books'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_04_011241) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_05_001113) do
   create_table "authors", force: :cascade do |t|
     t.string "author_name"
     t.text "author_bio"
@@ -31,6 +31,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_04_011241) do
     t.string "book_name"
     t.text "book_description"
     t.string "age_recomendations"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
