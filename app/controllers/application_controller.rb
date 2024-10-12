01f0b969bacb5027acb22429c8577731d2f96a87
Olesia Mironenko <olesia.a.mironenko@gmail.com>
2024-10-11 22:25:31 -0400

    login problem woked around

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  skip_before_action :authenticate_user!, only: [:index]
+  
   
 
   private
--- app/controllers/book_author_relation_controller.rb
+++ app/controllers/book_author_relation_controller.rb
@@ -1,5 +1,5 @@
 class BookAuthorRelationController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:show]
+  
 
   def index
   end
