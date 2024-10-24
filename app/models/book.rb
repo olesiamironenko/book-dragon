98e5ecb555ede761da823a18d4d414c5d6027773
Olesia Mironenko <olesia.a.mironenko@gmail.com>
2024-10-24 17:09:08 -0400

    book picture added, book edit fixed, profile picture fixed

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ApplicationRecord
     has_many :book_author_relations
     has_many :authors, through: :book_author_relations
+    has_one_attached :book_picture
     
     belongs_to :genre
 
@@ -11,4 +12,6 @@ class Book < ApplicationRecord
 
     validates :book_name, presence: true
     validates :age_recomendations, inclusion: { in: %w(babies kids youths adults) }
+    validates :book_picture, content_type: ['image/png', 'image/jpg', 'image/jpeg'],
+                              size: { less_than: 5.megabytes , message: 'should be less than 5MB' }
 end
